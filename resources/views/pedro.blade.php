12127bbcf39fd4e09244a9cf5a140963ef189ba6
Pedro Martinez <pmartinez1085@gmail.com>
2016-11-08 18:22:24 +0100

    pagina pedro v2

--- resources/views/pedro.blade.php
+++ resources/views/pedro.blade.php
@@ -1,7 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: pedro
- * Date: 8/11/16
- * Time: 18:05
- */
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+  Pàgina de Pedro
+@endsection
+
+
+@section('main-content')
+    <div class="container-fluid spark-screen">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Xuxman's Home</div>
+
+                    <div class="panel-body">
+                        Hello all, I'm Xuxman
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
