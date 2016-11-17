04e9cecb15b336793c23845713b796f094b5c008
Oscar Duran <xoscarduranx@gmail.com>
2016-11-17 20:18:29 +0100

    arreglos

---
+++ resources/views/sergi.blade.php
@@ -0,0 +1,22 @@
+@extends('adminlte::layouts.app')
+
+@section('htmlheader_title')
+    Pàgina de sergi
+@endsection
+
+
+@section('main-content')
+    <div class="container-fluid spark-screen">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Pàgina de sergi</div>
+
+                    <div class="panel-body">
+                        Hola que tal! Soc Sergi
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
